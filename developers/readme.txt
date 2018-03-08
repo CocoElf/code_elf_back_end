77891b50821cf87c442437b7e3e66f6898459dd7
151250145 <151250145@smail.nju.edu.cn>
2018-03-08 11:17:09 +0800

    feat<absPath>: add developer config
    请大家看一下 developers中的readme

---
+++ developers/readme.txt
@@ -0,0 +1,7 @@
+这个文件夹除了此文件（readme.txt）全部属于ignore文件
+这个文件的作用是为了让开发者可以自由使用自身开发环境的了路径，而不会导致git冲突
+
+本文件作为其余ignore文件路径
+developer是文件夹下应包含：
+developers.txt：1行，xxx为api在本机下所指的绝对路径
+absPath:xxx
--- src/main/java/cn/edu/nju/cocoelf/code_elf_back_end/config/file/FilePathConfig.java
+++ src/main/java/cn/edu/nju/cocoelf/code_elf_back_end/config/file/FilePathConfig.java
@@ -4,18 +4,46 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+
 
 @Configuration
 public class FilePathConfig extends WebMvcConfigurerAdapter {
-    public static final String API_PATH = "/Users/gyue/Downloads/api/python3";
+
+    public static Map<String,String> developerPror = new HashMap<>();
+
+    static {
+        try {
+            List<String> list =  Files.lines(Paths.get("developers/developers.txt")).collect(Collectors.toList());
+            for(String str : list){
+                String[] split = str.split(":");
+                developerPror.put(split[0],split[1]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     //供客户端使用的url前缀
     public static final String AVATAR_URL = "http://localhost:8081/api/";
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/api/**").addResourceLocations("file:" + API_PATH);
+
+        registry.addResourceHandler("/api/**").addResourceLocations("file:" + developerPror.get("absPath"));
         super.addResourceHandlers(registry);
+    }
+
+    public static void main(String[] args){
+        File file = new File("developers/absPath_zh_CN.properties");
+        System.out.println(file.getAbsolutePath());
+        System.out.println(file.exists());
+//        System.out.println(rb.getString("API_PATH"));
     }
 }
 
